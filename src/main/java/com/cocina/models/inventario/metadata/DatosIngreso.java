d65c47fceb5eba63bd77206ccaa09e6bf6c5f7f3
lacruzjd <lacruzjd@gmail.com>
2023-03-07 06:07:36 -0300

    entidades refactorizadas

--- src/main/java/com/cocina/models/inventario/metadata/DatosIngreso.java
+++ src/main/java/com/cocina/models/inventario/metadata/DatosIngreso.java
@@ -3,19 +3,9 @@ package com.cocina.models.inventario.metadata;
 import java.util.Date;
 
 public class DatosIngreso {
-    Date fechaIngreso;
-
-    public Date getFechaIngreso() {
-        return fechaIngreso;
-    }
-
-    public void setFechaIngreso(Date fechaIngreso) {
-        this.fechaIngreso = fechaIngreso;
-    }
-
-    @Override
-    public String toString() {
-        return "DatosIngreso [fechaIngreso=" + fechaIngreso + "]";
-    }
-
+    private Date fechaIngreso;
+    private Categorias categorias;
+    private Proveedor proveedor;
+    private UbicacionProducto ubicacionProducto;
+ 
 }
--- src/main/java/com/cocina/models/inventario/metadata/Proveedor.java
+++ src/main/java/com/cocina/models/inventario/metadata/Proveedor.java
@@ -1,10 +1,7 @@
 package com.cocina.models.inventario.metadata;
 
-public class Proveedor extends IdNombre {
+public class Proveedor {
     public Proveedor(String nombre) {
-        super(nombre);
-        //TODO Auto-generated constructor stub
+        
     }
-    String telefono;
-    String direccion;
 }
