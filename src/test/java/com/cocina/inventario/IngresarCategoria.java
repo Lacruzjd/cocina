e8936f78091f3944715fae9cd7313a33121207d4
lacruzjd <lacruzjd@gmail.com>
2023-03-07 16:39:46 -0300

    refactorizacion de entidades

---
+++ src/test/java/com/cocina/inventario/IngresarCategoria.java
@@ -0,0 +1,28 @@
+package com.cocina.inventario;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.cocina.models.inventario.Categoria;
+import com.cocina.viewmodels.inventario.categoriaAdminImpl;
+
+
+public class IngresarCategoria {
+
+    private categoriaAdminImpl categoria = new categoriaAdminImpl();
+
+    @Test
+    public void agregarCategoria() {
+    }
+
+    
+}
